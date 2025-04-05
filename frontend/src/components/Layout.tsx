38037b48d3ec16ad335ebc1c206493dc89bec8ff
Spandan-Mishra <spandanmishra69@gmail.com>
2025-04-05 11:46:11 +0530

    feedback, layout, sidebar added

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import { Toaster } from 'sonner';
+
+export default function Layout() {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto p-8">
+        <Outlet />
+      </main>
+      <Toaster position="top-right" />
+    </div>
+  );
+}
